15be696414467a345ffea32794194e5a07fef7de
Shiwen Han <hsw@Shiwens-MBP.attlocal.net>
2024-01-23 23:08:00 -0500

    basic function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,54 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
+// import { CreatePost } from "~/app/_components/create-post";
+import { CreateCard } from "~/app/_components/create-card";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
   noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
+  const cards = await api.card.getAll.query();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#dcdffc] to-[#babdfa] text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        {/* <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
+        </h1> */}
+
+        {/* Thanks To */}
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
+            href="https://weibo.com/u/7576045538"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
+            <h3 className="text-2xl font-bold">@蓝咕咕图像站 →</h3>
+            {/* <div className="text-lg">
               Just the basics - Everything you need to know to set up your
               database and authentication.
-            </div>
+            </div> */}
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
+            href="https://space.bilibili.com/28399376"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
+            <h3 className="text-2xl font-bold">@爱玩游戏的Penny →</h3>
+            {/* <div className="text-lg">
               Learn more about Create T3 App, the libraries it uses, and how to
               deploy it.
-            </div>
+            </div> */}
           </Link>
         </div>
+
+        {/* Log Info */}
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
+            {/* {hello ? hello.greeting : "Loading tRPC query..."} */}
           </p>
 
           <div className="flex flex-col items-center justify-center gap-4">
@@ -58,27 +64,54 @@ export default async function Home() {
           </div>
         </div>
 
-        <CrudShowcase />
+        <SubmitCard />
+
+        {/* Gallary */}
+        <div className=" grid grid-cols-2 gap-4 sm:grid-cols-4 md:gap-8">
+          {cards.map((card) => (
+            <div className=" " key={card.id}>
+              <Link className="" href={`/card/${card.id}`}>
+                <img
+                  className=" h-56 w-36 rounded-lg object-cover"
+                  src={card.image}
+                  alt={card.id}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        {/* Foot */}
+        <div className=" ">
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            href="https://github.com/TSHOGX"
+            target="_blank"
+          >
+            <div className=" text-sm">@TSHOGX</div>
+          </Link>
+        </div>
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
+async function SubmitCard() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+  // const latestPost = await api.post.getLatest.query();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
-      )}
+      )} */}
 
-      <CreatePost />
+      {/* <CreatePost /> */}
+      <CreateCard />
     </div>
   );
 }
